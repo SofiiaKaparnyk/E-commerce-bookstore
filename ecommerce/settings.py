ee86dad1dfd4d341bf6f24f500f8d05e0ca53db2
Sofiia Kaparnyk <omelianets.sofiya@gmail.com>
2020-07-01 18:41:16 +0300

    finish

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1ucb9y^7*(t(qmdrf@qnd5=w$#$ea8!-hfh02c(lgk0el=q%q)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
