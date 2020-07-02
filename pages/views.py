a7142c12ee07d796f9e61bbe2004234290e44387
Sofiia Kaparnyk <omelianets.sofiya@gmail.com>
2020-07-02 19:20:07 +0300

    mom

--- pages/views.py
+++ pages/views.py
@@ -4,7 +4,7 @@ from listings.models import Book, Category
 
 
 def index(request):
-    books = Book.objects.order_by('-created')[:6]
+    books = Book.objects.order_by('-created')[:12]
     categories = Category.objects.all()
     return render(request, 'pages/index.html', {'books': books, 'categories': categories})
 
